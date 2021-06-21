fe25a0aa1526ca2ff4a107744f3b536a537855a8
cre8 <brerfox33@gmail.com>
2021-06-21 12:12:43 +0300

    initial. Need to make some changes

---
+++ alice.py
@@ -0,0 +1,65 @@
+import os
+import re
+import subprocess
+
+from collections import OrderedDict
+from math import ceil
+
+
+class Alice:
+
+    def __init__(self, home):
+        # shell & aliases file path
+        self.home = home
+        self.config_path = f'{self.home}/.{str(os.environ["SHELL"][9:])}_aliases'
+
+    # @staticmethod
+    def get_aliases(self):
+        aliases = OrderedDict()
+        mode = 'r' if os.path.exists(self.config_path) else 'a+'
+        try:
+            with open(self.config_path, mode) as f:
+                for line in f.readlines():
+                    clean = line.replace('"', '')
+                    result = re.split(r'=', clean)
+                    name = result[0].replace('alias', '').lstrip()
+                    cmd = result[1].rstrip()
+                    aliases[name] = cmd
+            return aliases
+        except Exception as e:
+            raise e
+
+    def alias_source():
+        pass
+
+    def edit_aleases(self, editor):
+        mode = 'a'
+        initial_msg = False if os.path.exists(self.config_path) else True
+        #  try popen process to editor
+        try:
+            with open(self.config_path, mode) as f:
+                # if initial_msg:
+                #     f.write('# Write your aliases here')
+                subprocess.call([editor, self.config_path])
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def alias_paginate(ordered, page_counter: int):
+        alias_menu_page_counter = page_counter
+        pages = int(ceil(len(ordered) / 10))
+        if alias_menu_page_counter <= pages:
+            count = 0
+            chunk = {}
+            for key in ordered:
+                if not count == 0:
+                    if ((alias_menu_page_counter - 1) * 10) < count <= (alias_menu_page_counter * 10):
+                        chunk[f"{count}. {key}"] = ordered[key]
+                elif count == 0 and alias_menu_page_counter == 1:
+                    chunk[f"{count}. {key}"] = ordered[key]
+                count += 1
+
+            return chunk
+        else:
+            return 0
+
